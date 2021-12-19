4f4d83d88b58a7d1c80c6b9b9aa1f2c124a17da3
victorhugo1980 <93260292+victorhugo1980@users.noreply.github.com>
2021-12-19 18:24:38 +0100

    version2

--- supermercadojs.js
+++ supermercadojs.js
@@ -1,23 +1,26 @@
+//CONSTANTES
+ const items= document.getElementById('items');//div donde se van a pintar las cajas
+ const template_card= document.getElementById('template_card').content;//todo el contendido del div que se va a mostrar en el html
+ const fragment= document.createDocumentFragment();//es una memoria volatil que lo ocuparemos 
+
+
 //PRIMERO CARGA LA PAGINA Y LUEGO CAPTURAMOS LOS DATOS DEL ARCHIVO JSON
-document.addEventListener('DOMContentLoaded',() =>{
-  fetchData()
+document.addEventListener('DOMContentLoaded',() =>{//espera que cargue primero el DOM , con el DOMContentLoaded
+  fetchData()//una vez que lee el DOm ejecuta la función para capturar los datos del jSon
 })
 
 const fetchData =async()=>{
-  try{
-    const res=await fetch('supermercado.json');
-    const data= await res.json();
-    pintarCards(data);
+  try{//sino encuentra el jSon envia un error
+    const res=await fetch('supermercado.json');//await espera que lea primero el jSon 
+    const data= await res.json();//guardamos en data los datos del Json y espera hasta el documento este cargado para mostrarlos
+    pintarCards(data);//llama a la función para pintar las cajas y se le pasa data que son los elementos que vamos a pintar
   }catch(error){
     console.log(error);
   }
 }
-//ACCEDER A TEMPLATE
 
-const items= document.getElementById('items');
+//ACCEDER A TEMPLATE
 //const hijo= document.getElementById('card_body');
-const template_card= document.getElementById('template_card').content;
-const fragment= document.createDocumentFragment();
 const nombreBoton= document.getElementsByClassName('button').content;
 let contador={};
 //funcion abrir caja
@@ -30,29 +33,22 @@ const abrirCaja=e=>{
   //var s=template_card.querySelector('.button').dataset.id=e.target.dataset.id;//obtiene el id de la caja
   e.target.textContent="caja abierta";
   e.target.style.backgroundColor = "green";
-
-
-  //const contenido = document.getElementById('card_body');
- // const boton = document.createElement("button");
-           // boton.type = "button";
-           // boton.innerText = "Cerrando caja";
-           // boton.className = "button";
-            //boton.style.backgroundColor = "#F84300";
-           // let x = document.getElementById("caja_1");
-       
-      //contenido.appendChild(boton);
- // e.target.style.display="none";//oculta el boton
 const contadorCaja=objeto=>{
    console.log(objeto);
    const cuenta={
      id: objeto.querySelector('.button').dataset.id,
      cantidad:0,
-     estado:"cliente"
+     estado:"cliente",
+     inicio: 0,
+     fin:0
    }
 
    if(contador.hasOwnProperty(cuenta.id)){
      cuenta.cantidad=contador[cuenta.id].cantidad+1;
-     console.log(cuenta.cantidad);    
+     cuenta.inicio= new Date();
+  
+     console.log(cuenta.cantidad);  
+     console.log(cuenta.inicio)  
    }
 
    contador[cuenta.id]={...cuenta}
@@ -63,30 +59,23 @@ const contadorCaja=objeto=>{
 ///mirar que no fucniona
    const pintarContador=(e)=>{
      
-   /*  const boton = document.createElement("button");
-    boton.getElementsByClassName('button');
-    boton.innerText='cliente' */
-
      Object.values(contador).forEach(nCaja=>{ 
-      const con=document.getElementById(e.id)
+      const con=document.getElementById(e.id);
       con.textContent=nCaja.estado+' '+e.cantidad 
-    if(e.cantidad==1){
-         /*   const boton = document.createElement("button");
-           boton.type = "button";
-           boton.innerText = "atendiendo";
-           boton.classList="button";
-           boton.style.backgroundColor = "#F84300";
-           let x = document.getElementById("card_body");
-           x.appendChild(boton); */
-           return;
-      }
 
-      if(e.cantidad==4){
 
-        alert('caja llena')
+      if(e.cantidad===4){
+        //console.log(e.tiempo/1000)
+        nCaja.fin = new Date();
+        
+       console.log(nCaja.fin);
+     
+        let tiempoTranscurrido = (nCaja.fin-nCaja.inicio)/1000; //en milisegundos
+       // console.log(tiempoTranscurrido);
+        alert('tiempo transcurrido:' + tiempoTranscurrido);
+        alert('caja llena');
         nCaja.cantidad=0;
-        
-        
+                    
       }
     
      })
