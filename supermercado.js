672b5eb33e9c935ab25a34796edcc842f5261ec1
victorhugo1980 <93260292+victorhugo1980@users.noreply.github.com>
2021-12-21 13:50:48 +0100

    version

--- supermercado.js
+++ supermercado.js
@@ -31,10 +31,6 @@ const fetchData =async()=>{
     console.log(error);
   }
 }
-
-//ACCEDER A TEMPLATE
-//const hijo= document.getElementById('card_body');
-
 //ABRIR CAJA
 items.addEventListener('click',e=>{//aqui es donde se detecta el click
   abrirCaja(e);
@@ -73,14 +69,12 @@ if(SuperCliente.hasOwnProperty(cliente.id)){
 }
 SuperCliente[cliente.id]={...cliente};//TODO LO QUE ESTA EN EL OBJETO LO GURADO EN EL ARRAY PARA LUEGO PODER RECORRERLO
   console.log(SuperCliente);
-  //console.log(SuperCliente)
   pintarSuperCliente(cliente.id)
 }
 
 //let inicio;
 const pintarSuperCliente=(e)=>{
 
-  //console.log(SuperCliente)
   Object.values(SuperCliente).forEach((cliente)=>{  
      // console.log(e)
   if(cliente.id==e){
@@ -102,8 +96,6 @@ const pintarSuperCliente=(e)=>{
      alert(cliente.estado+" "+cliente.cantidad+' Caja llena')
      document.getElementById(cliente.id).textContent="";
      cliente.cantidad=-1;
-     
-
    /*   e=e-1;
     const botones=document.getElementsByClassName("button button_1")[e] */
     botones.textContent='Caja cerrada';
@@ -122,10 +114,9 @@ const pintarCards=data=>{
     //template_card.querySelector('button').textContent=caja.estado;
     template_card.querySelector('.button').dataset.id=caja.id;//se le asigna un id al boton
     template_card.querySelector('h2').id=caja.id;//se le da un id al h2
-
     const clone= template_card.cloneNode(true);
     fragment.appendChild(clone);
-       //console.log(caja);
+    //console.log(caja);
   })
   items.appendChild(fragment)
  
